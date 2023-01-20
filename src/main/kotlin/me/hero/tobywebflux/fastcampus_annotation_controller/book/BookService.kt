c4dfd4e58927171e1999bf6e59017c57cc760f0d
Hero <icraft2170@gmail.com>
2023-01-20 11:28:52 +0900

    Kotlin 확장 함수 toMono toFlux 사용해서 리팩터링

--- src/main/kotlin/me/hero/tobywebflux/fastcampus_annotation_controller/book/BookService.kt
+++ src/main/kotlin/me/hero/tobywebflux/fastcampus_annotation_controller/book/BookService.kt
@@ -3,6 +3,8 @@ package me.hero.tobywebflux.fastcampus_annotation_controller.book
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toFlux
+import reactor.kotlin.core.publisher.toMono
 import java.util.concurrent.atomic.AtomicInteger
 
 data class Book(val id: Int, val name: String, val price: Int)
@@ -11,20 +13,16 @@ data class Book(val id: Int, val name: String, val price: Int)
 class BookService {
 
     private final val nextId = AtomicInteger(0)
-    val books = mutableListOf<Book>(
+    val books: MutableList<Book> = mutableListOf<Book>(
         Book(id = nextId.incrementAndGet(), name = "Kotlin In Action", price = 30_000),
         Book(id = nextId.incrementAndGet(), name = "HTTP완벽 가이드", price = 40_000),
     )
     fun getAll(): Flux<Book> {
-        return Flux.fromIterable(books)
+        return books.toFlux()
     }
 
-    fun get(bookId: Int): Mono<Book> {
-        return Mono.justOrEmpty(
-            books.find { it.id == bookId }
-        )
-
-    }
+    fun get(bookId: Int): Mono<Book> =
+         books.find { it.id == bookId }.toMono()
 
     fun add(request: Map<String, Any>): Mono<Book> =
         Mono.just(request).map {map ->
